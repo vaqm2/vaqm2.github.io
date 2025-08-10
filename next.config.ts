07ec28f61298d1c00d49c227a48a2809486672fd
vaqm2 <appadurai.vivek@gmail.com>
2025-08-10 13:53:44 +0200

    Debug deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const repoName = "vaqm2.github.io"; // replace with your repo name if different
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
